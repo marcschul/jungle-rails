1e364232828aa39ca84eddb49db54f9c87b2f077
marcschul <marcschulm@gmail.com>
2022-01-10 18:37:17 +0000

    init Admin Dashbaord \w counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
 
   def show
+    @num_products = Product.count
+    @num_categories = Category.count
   end
+  
 end
