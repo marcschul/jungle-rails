fc3f772c3149884b6fa1efd6f4944059191ac413
marcschul <marcschulm@gmail.com>
2022-01-22 17:59:29 +0000

    fix bugs for product_spec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,39 +3,43 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
 
   describe 'Validations' do
-    it 'four_fields' do
-      @category = Category.create!(name: 'Blankets')
-      @product = @category.products.create!(name: 'fluffy', price: 7.99, quantity: 10)
+    it 'should create a product with the four fields specified' do
+      @category = Category.create(name: 'Blankets')
+      @product = @category.products.create(name: 'fluffy', price: 7.99, quantity: 10)
       @product.errors.full_messages
       expect(@product).to be_present
     end
 
-    it 'name' do
-      @category = Category.create!(name: 'Food')
-      @product = @category.products.create!(name: nil, price: 3.99, quantity: 100)
-      @product.errors.full_messages
-      expect(@product.name).to be_present
+    it 'should not create a product because of nil name' do
+      @category = Category.create(name: 'Food')
+      @product = @category.products.create(name: nil, price: 3.99, quantity: 100)
+      # puts @product.errors.full_messages
+      expect(@product.errors.full_messages.length).to be_truthy
+      expect(@product.id).to be_nil
     end
 
-    it 'price' do
-      @category = Category.create!(name: 'Plants')
-      @product = @category.products.create!(name: 'pickes', price: nil, quantity: 50)
-      @product.errors.full_messages
-      expect(@product.price).to be_present
+    it 'should not create a product because of nil price' do
+      @category = Category.create(name: 'Plants')
+      @product = @category.products.create(name: 'pickes', price: nil, quantity: 50)
+      # p @product.errors.full_messages
+      expect(@product.errors.full_messages.length).to be_truthy
+      expect(@product.id).to be_nil
     end
 
-    it 'quantity' do
-      @category = Category.create!(name: 'Vegetables')
-      @product = @category.products.create!(name: 'Tomatoes', price: 6.99, quantity: nil)
-      @product.errors.full_messages
-      expect(@product.quantity).to be_present
+    it 'should not create a product because of nil quantity' do
+      @category = Category.create(name: 'Vegetables')
+      @product = @category.products.create(name: 'Tomatoes', price: 6.99, quantity: nil)
+      # puts @product.errors.full_messages
+      expect(@product.errors.full_messages.length).to be_truthy
+      expect(@product.id).to be_nil
     end
 
-    it 'category' do
-      @category = Category.create!(name: nil)
-      @product = @category.products.create!(name: 'Charazard', price: 99.99, quantity: 1)
-      @product.errors.full_messages
-      expect(@product.category).to be_present
+    it 'should not create a product because of nil category' do
+      # @category = Category.create(name: nil)
+      @product = Product.create(name: 'Charazard', price: 99.99, quantity: 1)
+      # puts @product.errors.full_messages
+      expect(@product.errors.full_messages.length).to be_truthy
+      expect(@product.id).to be_nil
     end
   end
 end
