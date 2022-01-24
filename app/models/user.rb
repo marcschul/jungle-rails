63aa338e138def5213bf0a392bcf68e090d239a1
marcschul <marcschulm@gmail.com>
2022-01-24 23:25:20 +0000

    add user spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,20 @@ class User < ActiveRecord::Base
   
   has_secure_password
 
+  validates :name, presence: true
+  validates :password, confirmation: true, length: { minimum: 3 }
+  validates :email, uniqueness: { case_sensitive: false }
+  before_save { self.email.downcase! }
+
+  def self.authenticate_with_credentials(email, password)
+    #trim email parameter
+    strip_email = email.strip.downcase
+    user = User.find_by_email(strip_email)
+    if user && user.authenticate(password)
+      return user
+    else
+      return nil
+    end
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'Validations' do
+    # validation examples here
+    it 'should create a new user' do
+      @user = User.create(name: 'Flowers', email: 'flowers@email.com', password: nil)
+      expect(@user).to_not be_valid
+    end
+  end
+  
+  describe '.authenticate_with_credentials' do
+    # examples for this class method here
+    it 'should validate user credentials with spaces' do
+      @user = User.create(name: 'space_example', email: 'example@domain.com', password: '123')
+      input_email = '  example@domain.com  '
+      input_password = @user.password
+      expect(User.authenticate_with_credentials(input_email, input_password)).to eq(@user)
+    end
+  
+    it 'should create a new user, given input is upper and lower case' do
+      @user = User.create(name: 'uppper_lower_example', email: 'exAmple@domain.Com', password: '123')
+      input_email = 'eXample@domain.COM'
+      input_password = @user.password
+      expect(User.authenticate_with_credentials(input_email, input_password)).to eq(@user)
+    end
+  end
 end
